4e0bcbf9d4a578f5f12a28cdea6e2d844b0d8d92
unknown <snigdhadey.010@gmail.com>
2025-08-05 15:14:10 -0700

    fix: remove literal newline characters from about page JSX

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,8 @@ import AboutPageMain from '@/components/about/AboutPageMain'
 
 export default function AboutPage() {
     return (
-      <div className="min-h-screen bg-background">\n      <AboutPageMain /> \n    </div>
+      <div className="min-h-screen bg-background">
+        <AboutPageMain />
+      </div>
     )
   }
